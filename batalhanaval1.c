cc730cdf1a86ddd57bc22a111ca4a8770899c3c5
GodOfTruco <Gustavoicn792@gmail.com>
2025-05-02 01:11:33 -0300

    Update batalhanaval1.c

--- batalhanaval1.c
+++ batalhanaval1.c
@@ -1,32 +1,43 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-int main(){
-char linhas[10]= {'a','b','c','d','e','f','g','h','i','j'};
-int colunas[10]= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-int tabuleiro[10][10] = {0};
+int main() {
+    // Vetor com letras que representam as colunas (de 'a' a 'j')
+    char linhas[10] = {'a','b','c','d','e','f','g','h','i','j'};
 
-for (int i = 1; i < 4; i++){
-    tabuleiro[2][i] = 1;
-}
+    // Vetor com números que representam as linhas (de 1 a 10)
+    int colunas[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-for (int i = 1; i < 4; i++){
-    tabuleiro[i][6] = 1;
-}
+    // Matriz 10x10 inicializada com zeros, representando o tabuleiro
+    int tabuleiro[10][10] = {0};
 
-printf("tabuleiro de batalha naval\n");
-printf("  ");
-for (int l = 0; l < 10; l++){
-    printf("%c ", linhas[l]);
-}   
-printf("\n");
+    // Primeiro navio em linha horizontal (linha 2, colunas 1 a 3)
+    for (int i = 1; i < 4; i++) {
+        tabuleiro[2][i] = 1;
+    }
 
-for (int c = 0; c < 10; c++){
-    printf("%d ", colunas[c]);
-    for (int l = 0; l < 10; l++)
-    {
-        printf("%d", tabuleiro[c][l]);
+    // Segundo navio em linha vertical (coluna 6, linhas 1 a 3)
+    for (int i = 1; i < 4; i++) {
+        tabuleiro[i][6] = 1;
+    }
+
+    // Impressão do título do jogo
+    printf("tabuleiro de batalha naval\n");
+
+    // Impressão do cabeçalho das colunas (letras a-j)
+    printf("  ");
+    for (int l = 0; l < 10; l++) {
+        printf("%c ", linhas[l]);
     }
     printf("\n");
-}
-return 0;
+
+    // Impressão das linhas do tabuleiro, com números e conteúdo da matriz
+    for (int c = 0; c < 10; c++) {
+        printf("%d ", colunas[c]); // Número da linha (1 a 10)
+        for (int l = 0; l < 10; l++) {
+            printf("%d", tabuleiro[c][l]); // Conteúdo da célula
+        }
+        printf("\n"); // Nova linha
+    }
+
+    return 0;
 }
